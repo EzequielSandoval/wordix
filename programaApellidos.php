c6d1c2cb714d2ec6045e28457a0d84514b58d812
EzequielSandoval <sandovaleze3@gmail.com>
2022-11-11 13:27:58 -0300

    funcion cargar resumen + punto 9 exp 3

--- programaApellidos.php
+++ programaApellidos.php
@@ -70,6 +70,18 @@ function cargarPartidas()
     $coleccionPartidas[8] = ["palabraWordix" => "NAVES", "jugador" => "mario", "intentos" => 6, "puntaje" => 12];
     $coleccionPartidas[9] = ["palabraWordix" => "GOTAS", "jugador" => "mario", "intentos" => 1, "puntaje" => 16];
 }
+/**
+ * muestra el resumen de jugadores
+ * @return array
+ */
+function cargarResumenJugador()
+{
+    $resumenJugador = [];
+    $resumenJugador[0] = ["jugador" => "majo", "partidas" => 2, "puntaje" => 15, "victorias" => 1, "intento1" => 0, "intento2" => 0, "intento3" => 0, "intento4" => 1, "intento5" => 0, "intento6" => 1];
+    $resumenJugador[1] = ["jugador" => "rudolf", "partidas" => 2, "puntaje" => 30, "victorias" => 2, "intento1" => 0, "intento2" => 0, "intento3" => 2, "intento4" => 0, "intento5" => 0, "intento6" => 0];
+    $resumenJugador[2] = ["jugador" => "pink2000", "partidas" => 2, "puntaje" => 20, "victorias" => 1, "intento1" => 0, "intento2" => 0, "intento3" => 0, "intento4" => 1, "intento5" => 0, "intento6" => 1];
+    return ($resumenJugador);
+}
 
 //-------------------- PUNTO 3 DE LA EXPLICACION 3 --------------------
 /**
@@ -115,39 +127,70 @@ function seleccionarOpcion()
  * (agrega una nueva palabra al juego)
  * @return array 
  */
-function agregarPalabra ($colecPalabras,$palabraNueva){
+function agregarPalabra($colecPalabras, $palabraNueva)
+{
     //int $cantPalabras
-    $cantPalabras=count($colecPalabras);
-    $colecPalabras[$cantPalabras]=$palabraNueva;
+    $cantPalabras = count($colecPalabras);
+    $colecPalabras[$cantPalabras] = $palabraNueva;
     return $colecPalabras;
 }
 
+//--------------------PUNTO 9 EXPLICACION 3 --------------------
+/**
+ * muestra el resumen de un jugador especifico
+ * @param string $nombreJugador 
+ * @return array
+ */
+function resumenUnJugador($nombreJugador)
+{
+    // array $arrayJugadores
+    $arrayJugadores = cargarResumenJugador();
 
+    for ($i = 0; $i < count($arrayJugadores); $i++) {
+        if ($arrayJugadores[$i]["jugador"] == $nombreJugador) {
+            echo "*******************************";
+            echo "\nJugador: " . $arrayJugadores[$i]["jugador"];
+            echo "\nPartidas: " . $arrayJugadores[$i]["partidas"];
+            echo "\nPuntaje Total: " . $arrayJugadores[$i]["puntaje"];
+            echo "\nVictorias: " . $arrayJugadores[$i]["victorias"];
+            echo "\nPorcentaje de victorias: " . $arrayJugadores[$i]["victorias"] * 100 / $arrayJugadores[$i]["partidas"] . "%";
+            echo "\nAdivinadas: \n";
+            echo "  Intento 1: " . $arrayJugadores[$i]["intento1"] . "\n";
+            echo "  Intento 2: " . $arrayJugadores[$i]["intento2"] . "\n";
+            echo "  Intento 3: " . $arrayJugadores[$i]["intento3"] . "\n";
+            echo "  Intento 4: " . $arrayJugadores[$i]["intento4"] . "\n";
+            echo "  Intento 5: " . $arrayJugadores[$i]["intento5"] . "\n";
+            echo "  Intento 6: " . $arrayJugadores[$i]["intento6"] . "\n";
+            echo "*******************************";
+        }
+    }
+}
 /** --------------------- PUNTO 10 DE LA EXPLICACION 3 ---------------------
  * solicita al usuario el nombre de un jugador y lo retorna en minusculas
  * @param 
  * @return 
  */
 
-function solicitarJugador(){
+function solicitarJugador()
+{
     //string $nombreSolicitado
     echo "ingrese un nombre:\n";
     $soliNomb = trim(fgets(STDIN));
     $primLetra = str_split($soliNomb);
-    $vof= ctype_alpha($primLetra[0]);
+    $vof = ctype_alpha($primLetra[0]);
     //echo strlen($soliNomb)."\n";
-while ($vof == false){
-          echo "el nombre no es valido\n";
+    while ($vof == false) {
+        echo "el nombre no es valido\n";
         echo "ingrese otro nombre: ";
         $soliNomb = trim(fgets(STDIN));
         $primLetra = str_split($soliNomb);
-        $vof= ctype_alpha($primLetra[0]);
-     }
-   if ($vof == true) {
-            $soliNomb=strtolower($soliNomb);
-            echo "\n".$soliNomb;
-        }
- }
+        $vof = ctype_alpha($primLetra[0]);
+    }
+    if ($vof == true) {
+        $soliNomb = strtolower($soliNomb);
+        echo "\n" . $soliNomb;
+    }
+}
 
 
 /* ... COMPLETAR ... */
